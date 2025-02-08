61442a2d352a3677cd7aac913f2103de3b4ceca6
Vipul Anand <vipulanandd@gmail.com.com>
2025-02-08 20:54:27 +0530

    Implemented the Add functionality

--- context-todo/src/components/Form.jsx
+++ context-todo/src/components/Form.jsx
@@ -1,23 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTodo } from "../context/Todocontext";
 
-function handleSubmit() {
-  preventDefault();
-}
-
 const Form = () => {
+  const [todo, setTodo] = useState("");
+  const { addTodo } = useTodo();
+  const add = (e) => {
+    e.preventDefault();
+    if (!todo) return;
+    addTodo({
+      todo,
+      completed: false,
+    });
+    setTodo("");
+  };
   return (
     <div className="mb-4 p-4">
-      <form className="flex items-center space-x-4 w-full text-black">
+      <form
+        className="flex items-center space-x-4 w-full text-black"
+        onSubmit={add}
+      >
         <input
           type="text"
           className="flex-1 p-2 border border-gray-300 rounded-l-md"
           placeholder="Enter your TODO Here"
+          onChange={(e) => setTodo(e.target.value)}
         />
         <button
           type="submit"
           className="p-2 bg-blue-500 text-white rounded-r-md"
-          onSubmit={handleSubmit}
         >
           Add
         </button>
